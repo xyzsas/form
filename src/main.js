30bf368b9fe0263f01d7df6f052fadc4b50abcc2
CutePikachu <1019082574@qq.com>
2020-08-23 12:44:28 +1000

    Dev (#2)
    * basic form
    
    * basic form
    
    * basic form
    
    * fix form name validation issue
    
    * What R U doing
    
    Co-authored-by: Phantomlsh <phantomlsh@outlook.com>

--- src/main.js
+++ src/main.js
@@ -1,10 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import vuetify from '@/plugins/vuetify'
+import axios from 'axios'
 
 Vue.config.productionTip = false
 
+Vue.prototype.$ajax = axios.create({
+  baseURL: 'https://sas.aauth.link/'
+})
+
 new Vue({
+  vuetify,
   router,
   render: h => h(App)
 }).$mount('#app')
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,75 @@
 <template>
-  <v-form ref="form">
-  </v-form>
+  <div class="form">
+    <h1>{{ title }}</h1>
+    <p v-if="name">填写人：{{ name }}</p>
+    <v-form ref="form" style="width: 100%;">
+      <v-card v-for="(s, i) in form" :key="i" style="width: 100%; margin: 30px auto; padding: 10px 5%; max-width: 500px;">
+        <div v-for="(f, j) in s" :key="j">
+          <v-card-title v-if="f.title">{{ f.title }}</v-card-title>
+          <v-text-field v-if="f.input" outlined dense v-model="input[f.input]" :label="f.input" :type="f.type || 'text'" :rules="[rule(f, f.input)]"></v-text-field>
+          <v-select v-if="f.select" outlined dense v-model="input[f.select]" :items="f.items" :label="f.select" :rules="[rule(f, f.select)]"></v-select>
+        </div>
+      </v-card>
+      <v-btn v-if="form" color="primary" large @click="submit" :loading="loading">提交</v-btn>
+    </v-form>
+  </div>
 </template>
 
 <script>
-// @ is an alias to /src
-// import HelloWorld from '@/components/HelloWorld.vue'
+import form from '../form.js'
+
+const SS = window.sessionStorage
 
 export default {
-  name: 'Home'
+  name: 'Home',
+  data: () => ({
+    loading: false,
+    name: SS.name,
+    form: null,
+    title: '新生信息采集',
+    input: {}
+  }),
+  mounted () {
+    if (!SS.token) {
+      window.location.href = '/user/#/?c=/form/'
+      this.title = '请先登录'
+    } else {
+      this.render(form)
+    }
+  },
+  methods: {
+    render (form) {
+      this.form = []
+      for (const f of form) {
+        if (f.title) this.form.push([f])
+        else this.form[this.form.length - 1].push(f)
+      }
+    },
+    rule (f, key) {
+      if (f.optional) return true
+      if (!this.input[key]) return '必填'
+      else return true
+    },
+    async submit () {
+      if (!this.$refs.form.validate()) return
+      this.loading = true
+      // here
+    }
+  }
 }
 </script>
+
+<style scoped>
+  div.form {
+    padding: 30px;
+    color: #333;
+    background: #F2F2F2;
+    width: 100%;
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+  }
+</style>
