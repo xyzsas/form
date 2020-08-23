142809a2472fa236d4418718c07566fc9afcdc4b
Phantomlsh <phantomlsh@outlook.com>
2020-08-22 19:31:02 -0700

    What R U doing

---
+++ src/form.js
@@ -0,0 +1,55 @@
+export default [
+  {
+    title: '学生个人信息'
+  },
+  {
+    select: '性别',
+    items: ['男', '女']
+  },
+  {
+    input: '毕业学校'
+  },
+  {
+    input: '中考分',
+    type: 'digit'
+  },
+  {
+    input: '中考准考证号',
+    type: 'digit'
+  },
+  {
+    input: '民族'
+  },
+  {
+    input: '现住址'
+  },
+  {
+    title: '家长信息'
+  },
+  {
+    input: '父亲姓名'
+  },
+  {
+    input: '父亲年龄',
+    type: 'digit'
+  },
+  {
+    input: '父亲工作单位'
+  },
+  {
+    input: '父亲联系电话'
+  },
+  {
+    input: '母亲姓名'
+  },
+  {
+    input: '母亲年龄',
+    type: 'digit'
+  },
+  {
+    input: '母亲工作单位'
+  },
+  {
+    input: '母亲联系电话'
+  }
+]
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,65 +1,59 @@
 <template>
   <div class="form">
-    <h1>{{ formTitle }}</h1>
-    <v-form ref="form" style="width: 80%; max-width: 400px">
-     <template v-for="(i, indexI) in formData">
-      <v-card-title v-if="i.field === 'title'" :key="indexI">{{ i.name }}</v-card-title>
-      <v-text-field
-        v-if="i.field === 'input'"
-        :key="indexI"
-        v-model="inputData[i.name]"
-        :label="i.name"
-        :rules="i.rules"
-        :type="i.type"
-      ></v-text-field>
-      <v-radio-group
-        v-model="inputData[i.name]"
-        v-if="i.field === 'radio'"
-        :key="indexI"
-        :name="i.name"
-        :rules="i.rules"
-        :label="i.name"
-        mutiple
-      >
-        <v-radio
-          v-for="(j, indexJ) in i.item"
-          :key="indexJ"
-          :label="j"
-          :value="j"
-        ></v-radio>
-      </v-radio-group>
-      <v-select
-        v-if="i.field === 'select'"
-        :key="indexI"
-        v-model="inputData[i.name]"
-        :rules="i.rules"
-        :items="i.items"
-        :label="i.name"
-        multiple
-        required
-      ></v-select>
-     </template>
+    <h1>{{ title }}</h1>
+    <p v-if="name">填写人：{{ name }}</p>
+    <v-form ref="form" style="width: 100%;">
+      <v-card v-for="(s, i) in form" :key="i" style="width: 100%; margin: 30px auto; padding: 10px 5%; max-width: 500px;">
+        <div v-for="(f, j) in s" :key="j">
+          <v-card-title v-if="f.title">{{ f.title }}</v-card-title>
+          <v-text-field v-if="f.input" outlined dense v-model="input[f.input]" :label="f.input" :type="f.type || 'text'" :rules="[rule(f, f.input)]"></v-text-field>
+          <v-select v-if="f.select" outlined dense v-model="input[f.select]" :items="f.items" :label="f.select" :rules="[rule(f, f.select)]"></v-select>
+        </div>
+      </v-card>
+      <v-btn v-if="form" color="primary" large @click="submit" :loading="loading">提交</v-btn>
     </v-form>
-    <v-btn outlined @click="submit">确认提交</v-btn>
-    <br>
   </div>
 </template>
 
 <script>
-import formData from '../data/stuReg'
-import swal from 'sweetalert'
+import form from '../form.js'
+
+const SS = window.sessionStorage
 
 export default {
   name: 'Home',
   data: () => ({
-    formData,
-    formTitle: '新生报到注册',
-    inputData: {}
+    loading: false,
+    name: SS.name,
+    form: null,
+    title: '新生信息采集',
+    input: {}
   }),
+  mounted () {
+    if (!SS.token) {
+      window.location.href = '/user/#/?c=/form/'
+      this.title = '请先登录'
+    } else {
+      this.render(form)
+    }
+  },
   methods: {
-    submit () {
+    render (form) {
+      this.form = []
+      for (const f of form) {
+        if (f.title) this.form.push([f])
+        else this.form[this.form.length - 1].push(f)
+      }
+    },
+    rule (f, key) {
+      if (f.optional) return true
+      if (!this.input[key]) return '必填'
+      else return true
+    },
+    async submit () {
       if (!this.$refs.form.validate()) return
-      swal('提交成功！', '感谢您的参与！')
+      this.loading = true
+      // here
     }
   }
 }
@@ -67,9 +61,11 @@ export default {
 
 <style scoped>
   div.form {
+    padding: 30px;
     color: #333;
     background: #F2F2F2;
     width: 100%;
+    min-height: 100vh;
     display: flex;
     flex-direction: column;
     justify-content: center;
